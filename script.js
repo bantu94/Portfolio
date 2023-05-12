52dc52099f9601964a5a25770a0ee565a1878e37
Matto <mattoloma@gmail.com>
2023-05-12 21:48:54 +0300

    create & customize services

--- script.js
+++ script.js
@@ -198,3 +198,21 @@ projectsBtn.addEventListener("click", (e) => {
   showHideBool = !showHideBool;
 });
 // End of Project button 
+
+// Section 4
+document.querySelectorAll(".service__btn").forEach((service) => {
+  service.addEventListener("click", e => {
+    e.preventDefault();
+
+    const serviceText = service.nextElementSibling;
+    serviceText.classList.toggle("change")
+
+    const rightPosition = serviceText.classList.contains("change") 
+    ? `calc(100% - ${getComputedStyle(service.firstElementChild).width})` 
+    : 0;
+
+    service.firstElementChild.style.right = rightPosition;
+
+  })
+})
+// End  of Section 4
