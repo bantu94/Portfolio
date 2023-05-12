5fd5e3020242b6957d50882d580cce37427b8047
Matto <mattoloma@gmail.com>
2023-05-12 10:35:48 +0300

    create and customize projects

--- script.js
+++ script.js
@@ -109,16 +109,43 @@ Array.from(aboutMeTextContent).forEach((char) => {
 // end of About me Text 
 
 // Projects
+const container = document.querySelector('.container');
 const projects = document.querySelectorAll('.project');
+const projectHideBtn = document.querySelector('.project__hide_btn')
 
 projects.forEach((project) => {
   project.addEventListener('mouseenter', () => {
     project.firstElementChild.style.top = `-${project.firstElementChild.offsetHeight - project.offsetHeight + 20}px`
   });
 
-  project.addEventListener('mouseleave', () => {
+  project.addEventListener("mouseleave", () => {
     project.firstElementChild.style.top = "2rem"
   });
+
+  // Big project image 
+  project.addEventListener("click", () => {
+    const bigImgWrapper = document.createElement("div");
+    bigImgWrapper.className = "project__img_wrapper";
+    container.appendChild(bigImgWrapper);
+
+    const bigImg = document.createElement("img");
+    bigImg.className = "project__img";
+    const imgPath = project.firstElementChild.getAttribute("src").split(".")[0];
+    bigImg.setAttribute("src", `${imgPath}.jpeg`);
+    bigImgWrapper.appendChild(bigImg);
+
+    document.body.style.overflowY = "hidden";
+
+    projectHideBtn.classList.add("change")
+    projectHideBtn.onclick = () => {
+      projectHideBtn.classList.remove("change")
+      bigImgWrapper.remove()
+      document.body.style.overflowY = "scroll";
+    }
+
+
+  });
+  // End of Big project image 
 
 });
 // End of Projects
