5b6493714b15d17d5b4623c37f800df1fb1b8a4f
Matto <mattoloma@gmail.com>
2023-05-13 12:10:14 +0300

    social media slide show

--- script.js
+++ script.js
@@ -241,4 +241,23 @@ formInputs.forEach((input) => {
 
 });
 
+const slideshow = document.querySelector(".slideshow") 
+
+setInterval(() => {
+  const firstIcon = slideshow.firstElementChild;
+  firstIcon.classList.add("faded__out");
+
+  const thirdIcon = slideshow.children [3];
+  thirdIcon.classList.add("light");
+  thirdIcon.previousElementSibling.classList.remove("light");
+
+  setTimeout(() => {
+    slideshow.removeChild(firstIcon);
+    slideshow.appendChild(firstIcon);
+    setTimeout(() => {
+      firstIcon.classList.remove("faded__out");
+    }, 500);
+  }, 500);
+}, 3000);
+
 // End  of Section 5
